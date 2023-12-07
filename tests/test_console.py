f1168d9eb6b05d2e7cf5f41df15fe69f5f693efc
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-12-07 03:04:35 +0000

    test for empty line pass

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,10 +12,17 @@ Unittest classes:
 """
 import unittest
 from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestHBNBCommand_prompting(unittest.TestCase):
     """Unittests for testing prompting of the HBNB command interpreter."""
 
     def test_prompt_string(self):
-        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+        self.assertEqual("(alxbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
