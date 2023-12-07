92b3cdfb7dc9b665d5b8429ee7bfd3e3befb2fc8
George S. Mulbah II <gsmulbah2500@gmail.com>
2023-12-07 03:58:52 +0000

    passed the quit command test

--- tests/test_console.py
+++ tests/test_console.py
@@ -26,3 +26,9 @@ class TestHBNBCommand_prompting(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd(""))
             self.assertEqual("", output.getvalue().strip())
+
+    def test_help_quit(self):
+        h = "Quit command to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(h, output.getvalue().strip())
