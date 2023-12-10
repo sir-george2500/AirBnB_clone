bfc3499400a9b08912912163aa190f077b6a35e7
George S. Mulbah <gsmulbah2500@gmail.com>
2023-12-10 22:21:23 +0000

    added the base class doc

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
 import unittest
 import sys
 import os
